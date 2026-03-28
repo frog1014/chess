ee8fd226e5abfba207156bdeebfc3dd22af145c9
Chess Developer <dev@chess.local>
2026-03-28 22:16:56 +0800

    feat: convert project to TypeScript

--- App.js
+++ App.js
@@ -1,24 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { I18nextProvider } from 'react-i18next';
-import './src/i18n'; // 初始化 i18n
-import i18n from './src/i18n';
-import { PlayScreen } from './src/screens/PlayScreen';
+import App from './App.tsx';
+export default App;
 
-export default function App() {
-  return (
-    <I18nextProvider i18n={i18n}>
-      <View style={styles.container}>
-        <PlayScreen />
-        <StatusBar style="auto" />
-      </View>
-    </I18nextProvider>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
